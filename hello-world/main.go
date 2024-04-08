adecfc039b1bcc733d6e981fe4864c97f9c0e8a7
Gabriel Brogni Zaccaron <gabrielbrogni@hotmail.com>
2024-04-07 22:02:08 -0300

    add support for other languages

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,15 +2,25 @@ package main
 
 import "fmt"
 
+const spanishWord = "Spanish"
+const frenchWord = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanishWord {
+		return spanishHelloPrefix + name
+	}
+	if language == frenchWord {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
