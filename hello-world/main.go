ac76e4c24fbf903331cf9d2558c4f16a779142ff
Gabriel Brogni Zaccaron <gabrielbrogni@hotmail.com>
2024-04-07 22:26:24 -0300

    refactor to use switch

--- hello-world/main.go
+++ hello-world/main.go
@@ -12,13 +12,19 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanishWord {
-		return spanishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case frenchWord:
+		prefix = frenchHelloPrefix
+	case spanishWord:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	if language == frenchWord {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
