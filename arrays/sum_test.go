42c4fda40c0a3a66908cab652fef5e547f5b8db0
Gabriel Brogni Zaccaron <gabrielbrogni@hotmail.com>
2024-04-09 23:38:40 -0300

    add sum all func

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -15,6 +16,15 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
 
 func BenchmarkSum(b *testing.B) {
